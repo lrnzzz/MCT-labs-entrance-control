7cfe2cb96b1df3f01d27f40466fb53d0fc899803
Lorenz Adriaensen <lorenz.adriaensen@kdg.be>
2014-12-11 16:05:15 +0100

    Added php API

---
+++ api/README.md
@@ -0,0 +1,1 @@
+### This repo is no longer maintained. Development has continued over at [ArrestDB](https://github.com/alixaxel/ArrestDB)
---
+++ api/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$db_config = array(
+    'server'   => 'localhost', // Usually localhost
+    'database' => 'deb89019_mctlab', // The database name
+    'username' => 'deb89019_mctlab', // The database username
+    'password' => 'KdG10119023', // The database password
+    'verbose' => false // If true errors will be shown
+);
+
+?>
